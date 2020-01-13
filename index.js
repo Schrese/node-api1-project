da6b5241edac85f89145a911cc5fecfd2a577c19
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-13 17:01:18 -0500

    Just getting everything set up

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+//GET users (find())
+
+//GET users by id (findById())
+
+//POST new user (insert())
+
+//PUT existing user by id (update())
+
+//Delete user by id (remove())
+
+
+const port = 8000;
+server.listen(port, () => console.log('\n ** api on port : ${port} \n'))
