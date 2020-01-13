fa3de427d76fd98d07039cb2d5b943a85f34fdf8
Schrese <53787633+Schrese@users.noreply.github.com>
2020-01-13 17:07:08 -0500

    fetched all users

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 // implement your API here
 const express = require('express');
 
+const Users = require('./data/db.js')
+
 const server = express();
 
 server.use(express.json());
 
 //GET users (find())
+server.get('/', (req, res) => {
+    Users.find()
+        .then(users => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            console.log('error getting all users', error)
+            res.status(500).json({errorMessage: 'The users information could not be retrieved.'})
+        })
+})
 
 //GET users by id (findById())
 
